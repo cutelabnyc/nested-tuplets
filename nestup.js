3da668fedc581fcb2c0ccb6380c67f779481b8d7
Sam Tarakajian <sam@cycling74.com>
2020-10-28 15:05:28 -0400

    adjusted Max interface

--- nestup.js
+++ nestup.js
@@ -1,12 +1,27 @@
 const maxApi = require("max-api");
 const RhythmParser = require("./src/parser");
-const NestedTuplet = require("./src/nestedTuplet");
+const Phrase = require("./src/phrase");
+
+function normalizedOnsetTimes(phrases) {
+	// Collect the total number of beats
+	const totalBeats = phrases.reduce((prev, {length, beatRatio}) => prev + length * beatRatio, 0);
+
+	// Scale the normalized onset times in each phrase by the beat length in each phrase
+	let beatOffset = 0;
+	let onsets = [];
+	phrases.forEach(p => {
+		const { length, beatRatio, normalizedOnsetTimes } = p;
+		const scaledOnsetTimes = normalizedOnsetTimes.map(t => t / (length * beatRatio) + beatOffset);
+		beatOffset += (1.0 / (length * beatRatio));
+		onsets = onsets.concat(scaledOnsetTimes);
+	});
+}
 
 function process(text) {
 	const parser = new RhythmParser();
 	const results = parser.parse(text);
-	const nt = new NestedTuplet(results);
-	const times = nt.normalizedOnsetTimes();
+	const phrases = results.map(r => new Phrase(r));
+	const times = normalizedOnsetTimes(phrases);
 	return times;
 }
 
