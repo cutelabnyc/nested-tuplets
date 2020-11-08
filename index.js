862efc4f4e7b045828eb06c1e3a08732930dbdae
Sam Tarakajian <sam@cycling74.com>
2020-11-08 08:21:11 -0500

    added npmignore

--- index.js
+++ index.js
@@ -1,68 +1,11 @@
-const fs = require("fs");
-const path = require("path");
 const RhythmParser = require("./src/parser");
-const NestedTuplet = require("./src/nestedTuplet");
-const writeTimesAsMidi = require("./src/trackWriter");
-const { program } = require('commander');
-program.version('0.1.0');
+const Phrase = require("./src/phrase");
+const ParseError = require("./src/parseError");
+const { normalizedOnsets } = require("./src/util");
 
-program
-	.requiredOption('-i, --input <input>', 'input rhythm file (required)')
-	.option('-w, --watch <destination>', 'output directory for watch mode')
-	.parse(process.argv);
-
-function process(text, outputFilePath) {
-	const parser = new RhythmParser();
-	const results = parser.parse(text);
-	const nt = new NestedTuplet(results);
-	const times = nt.normalizedOnsetTimes();
-	writeTimesAsMidi(times, outputFilePath);
-}
-
-function makeOutName(container, prefix) {
-	if (container) {
-		const outName = `out-${Date.now()}`;
-		return path.join(container, outName);
-	} else {
-		return prefix;
-	}
-}
-
-let outName = makeOutName(program.watch, program.args[0]);
-let lastModTime = fs.statSync(program.input).mtime;
-
-// Process straight through
-console.log(program.input);
-fs.readFile(program.input, { encoding: "utf8" }, (err, contents) => {
-	if (err) throw err;
-	process(contents, outName);
-});
-
-// Watch mode
-if (program.watch) {
-	let location = path.dirname(program.input);
-	console.log(`Watching ${program.input} for file changes`);
-	fs.watch(
-		program.input,
-		{ persistent: true, encoding: "utf8" },
-		(eventType, filename) => {
-			if (eventType === "change") {
-				const inpath = path.join(location, filename);
-				const nmodtime = fs.statSync(inpath).mtime;
-				if (nmodtime.getTime() !== lastModTime.getTime()) {
-					lastModTime = nmodtime;
-					outName = makeOutName(program.watch, program.args[0]);
-					console.log(`Change detected, writing to ${outName}`);
-					fs.readFile(path.join(location, filename), { encoding: "utf8" }, (err, contents) => {
-						if (err) throw err;
-						try {
-							process(contents, outName);
-						} catch (e) {
-							console.error(e);
-						}
-					});
-				}
-			}
-		}
-	)
-}
+module.exports = {
+	RhythmParser,
+	Phrase,
+	ParseError,
+	normalizedOnsets
+};
