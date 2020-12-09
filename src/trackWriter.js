f36211a1c38fa4fdf4ac61900a52df9900777bf5
Sam Tarakajian <sam@cycling74.com>
2020-12-09 11:44:45 -0500

    track writer uses the new on/off events system

--- src/trackWriter.js
+++ src/trackWriter.js
@@ -3,13 +3,29 @@ const midiWriter = require("midi-writer-js");
 module.exports = function writeOnsetsAsMidi(nestup, pitch, ticks, outfile) {
 	// Assume 4 beats to one measure
 	const track = new midiWriter.Track();
-	const tickedOnsets = nestup.tickSnappedEvents(ticks);
-	tickedOnsets.forEach((onset, idx) => {
-		console.log(onset);
-		track.addEvent(new midiWriter.NoteEvent({
-			pitch: pitch,
-			duration: "T" + onset.duration
-		}));
+	const midiLikeEvents = nestup.asOnOffEvents(ticks);
+	let lastEvent = null;
+
+	let tickCounter = 0;
+	let noteStartTick = 0;
+	midiLikeEvents.forEach((event) => {
+
+		if (event.on) {
+			noteStartTick = event.time;
+		}
+
+		else {
+			const duration = event.time - noteStartTick;
+			const wait = noteStartTick - tickCounter;
+
+			track.addEvent(new midiWriter.NoteEvent({
+				pitch: pitch,
+				duration: "T" + duration,
+				wait: "T" + wait
+			}));
+
+			tickCounter += (wait + duration);
+		}
 	});
 
 	const write = new midiWriter.Writer(track);
